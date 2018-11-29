2fea41e5fb473131dbbb2cb241cbe1934ab9a529
Florian <flointex@gmail.com>
2018-11-29 15:05:52 +0100

    Working Product

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/layout/NavBar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Index from './components/layout/Index'
+import { Provider } from './context'
+import Lyrics from './components/tracks/Lyrics'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider>
+                <Router>
+                    <React.Fragment>
+                        <Navbar />
+                        <div className="container">
+                            <Switch>
+                                <Route exact path="/" component={Index} />
+                                <Route path="/lyrics/track/:id" component={Lyrics} />
+                            </Switch>
+                        </div>
+                    </React.Fragment>
+                </Router>
+            </Provider>
+        )
+    }
 }
 
-export default App;
+export default App
