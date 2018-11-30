c056547c4856ddc9bc94425f8551e16f2f71e03e
Florian <flointex@gmail.com>
2018-11-30 11:51:12 +0100

    new build

---
+++ build/precache-manifest.eeefa4922d2269541e73dca0ed39d2c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e7ce372bdd1093f1d7b",
+    "url": "/lyrics-finder/static/css/main.9743dcc1.chunk.css"
+  },
+  {
+    "revision": "8e7ce372bdd1093f1d7b",
+    "url": "/lyrics-finder/static/js/main.8e7ce372.chunk.js"
+  },
+  {
+    "revision": "ef5d50860d4f226eddf4",
+    "url": "/lyrics-finder/static/js/1.ef5d5086.chunk.js"
+  },
+  {
+    "revision": "e4bbd3c3f241158cbe70",
+    "url": "/lyrics-finder/static/js/runtime~main.e4bbd3c3.js"
+  },
+  {
+    "revision": "a8b2db1ef4718d18ced11f9d4ebb780f",
+    "url": "/lyrics-finder/index.html"
+  }
+];
